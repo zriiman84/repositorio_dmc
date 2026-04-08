2944ee4e0b96819df474ef97ecf4df45c285fdf6
Cristofer_Vargas <crisric84@gmail.com>
2026-04-08 18:07:43 -0500

    feat(calculadora): Crear un módulo principal como guía para ejemplificar el uso de la clase Calculadora

---
+++ package_calculadora/principal.py
@@ -0,0 +1,11 @@
+from calculadora import Calculadora
+
+print("Calculadora de Python")
+print('Sumar (6,3): ', Calculadora.sumar(6,3)) 
+print('Restar (6,3): ', Calculadora.restar(6,3))
+print('Dividir (6,3): ', Calculadora.dividir(6,3))
+print('Multiplicar (6,3): ', Calculadora.multiplicar(6,3))
+#Elevar 6 a la potencia 3 
+print('Potenciar (6,3): ', Calculadora.exponenciar(6,3)) 
+#Obtener la raíz cúbica de 6
+print('Raíz (6,3): ', Calculadora.obtener_raiz(6,3))
