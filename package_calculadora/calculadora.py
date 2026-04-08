bba7fb20b4621a87e3e4f74314f3692529b30d10
Cristofer_Vargas <crisric84@gmail.com>
2026-04-08 18:05:27 -0500

    feat(calculadora): Crear una clase Calculadora para realizar operaciones básicas (SUMA, RESTA, MULTIPLICACIÓN, DIVISIÓN, POTENCIA Y RADICACIÓN) con 2 números como input

---
+++ package_calculadora/calculadora.py
@@ -0,0 +1,54 @@
+import math
+
+class Calculadora:
+    
+    @staticmethod
+    # Función que suma dos números
+    def sumar(a : int|float, b : int|float) -> int|float:
+        if(isinstance(a,(int,float)) and isinstance(b,(int,float)) ):
+            return a + b
+        else:
+            raise ValueError("Debe ingresar valores numéricos enteros o flotantes.")
+
+    @staticmethod
+    # Función que resta dos números
+    def restar(a : int|float, b : int|float) -> int|float:
+        if(isinstance(a,(int,float)) and isinstance(b,(int,float))):
+            return a - b
+        else:
+            raise ValueError("Debe ingresar valores numéricos enteros o flotantes.")
+
+    @staticmethod
+    # Función que multiplica dos números
+    def multiplicar(a : int|float, b : int|float) -> int|float:
+        if(isinstance(a,(int,float)) and isinstance(b,(int,float)) ):
+            return a * b
+        else:
+            raise ValueError("Debe ingresar valores numéricos enteros o flotantes.")
+
+    @staticmethod
+    # Función que divide dos números
+    def dividir(a : int|float, b : int|float) -> int|float:
+        if(isinstance(a,(int,float)) and isinstance(b,(int,float))):
+            if b == 0:
+                raise ValueError("No se puede dividir entre 0")
+            return a / b
+        else:
+            raise ValueError("Debe ingresar valores numéricos enteros o flotantes.")
+        
+    @staticmethod
+     # Función que sirve para exponenciar dos números
+    def exponenciar(a : int|float, b : int|float) -> int|float:
+        if(isinstance(a,(int,float)) and isinstance(b,(int,float))):
+            return math.pow(a,b)
+        else:
+            raise ValueError("Debe ingresar valores numéricos enteros o flotantes.")
+        
+    @staticmethod
+     # Función para obtener raíz de dos números
+    def obtener_raiz(a : int|float, b : int|float) -> int|float:
+        if(isinstance(a,(int,float)) and isinstance(b,(int,float))):
+            return round(math.pow(a,1/b),4)
+        else:
+            raise ValueError("Debe ingresar valores numéricos enteros o flotantes.")
+        
